b88b1bc99bec2600ff6f7556a65aa24abc69a68c
Artem Dymov <dymov.artem@outlook.com>
2023-12-15 16:32:57 +0200

    settings_example.py added

---
+++ settings_example.py
@@ -0,0 +1,18 @@
+BOT_TOKEN = 'TOKEN'
+# DATABASE_URL = 'postgresql://postgres:password@localhost:5432/database'
+
+# DB data
+database_data = {
+    'host': 'localhost',
+    'user': 'postgres',
+    'password': '0000',
+    'database': 'name',
+    'port': 5432
+}
+
+# 1 - registration, 2 - second phase (after reg)
+mode = 1
+
+end_reg_date = '20.12.2023'
+end_gift_date = '28.12.2023'
+admin_id = 0000
