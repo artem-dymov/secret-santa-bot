04e2f43c5f4fc8c6d52bcc9b6c09adaca82c5ddf
Artem Dymov <dymov.artem@outlook.com>
2023-12-15 18:09:07 +0200

    imports changes

--- src/keyboards.py
+++ src/keyboards.py
@@ -1,6 +1,6 @@
 from aiogram.utils.keyboard import InlineKeyboardMarkup, InlineKeyboardButton
 
-from config import start_cmd_message
+from src.config import start_cmd_message
 
 start_agreement_kb = InlineKeyboardMarkup(inline_keyboard=[[
     InlineKeyboardButton(text='Погоджуюсь', callback_data='initial_agreement')
